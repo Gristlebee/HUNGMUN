9eef9295fcf62996cde237da4b5509daf29daef7
Gristlebee <56049844+Gristlebee@users.noreply.github.com>
2020-02-24 15:06:07 -0800

    Add files via upload

--- Hangman - Student Copy/Puzzle.java
+++ Hangman - Student Copy/Puzzle.java
@@ -6,27 +6,64 @@ import java.io.FileNotFoundException;
 public class Puzzle
 {
     
-    String correct="correct";
-    String[] A = new String[correct.length()];
+    ArrayList<String> words;
+    public Puzzle() {
+         words = new ArrayList<String>();
+
+        try {
+            File file = new File("words.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()) {
+                String tempWord = scanner.next().toUpperCase();
+                words.add(tempWord);
+            }
+            scanner.close();
+		
+	
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    String [] correct=wordD(words.get((int)(Math.random())*(words.size())));
+    String[] A = new String[correct.length];
     String[] Wrg = new String[100];
+    String 
     int wrg=0;
+    public String[] wordD(String s) {
+        String[] a= new String[s.length()];
+        for(int i=0; i<s.length(); i++){
+            a[i]=s.charAt(i)+"";
+        }
+        return a;
+    }
      public void set() {
          for(int i=0; i<Wrg.length; i++) {
             Wrg[i]="";
-         
+            
         }
-    }
-    public boolean isUnsolved() {
-      
-        return true;
-     
-    }
-    public void show() {
-        String[] A = new String[correct.length()];
-        
         for(int i=0; i<A.length; i++) {
          A[i]="_";
         }
+    }
+    public boolean isUnsolved() {
+         for(int i=0; i<A.length;i++){
+           
+          if(A[i].equals("_")){
+               System.out.println("LETS PLAY HUNGMUN");
+             return true;
+             
+            } 
+        }
+       
+        return false;
+     
+    }
+    public void show() {
+        
+        
+        
          for(int i=0; i<A.length; i++) {
          System.out.print(A[i]);
         }
@@ -39,14 +76,16 @@ public class Puzzle
        
     }
     public boolean makeGuess(String guess) {
+        int yes=0;
         for(int i=0; i<correct.length();i++){
             String s=correct.substring(i,i+1);
           if(guess.equals(s)){
-              return true;
+              A[i]=guess;
+              yes=1;
               
             } 
         }
-        
+        if(yes==1) {return true;}
         Wrg[wrg]=guess;
         wrg++;
         return false;
